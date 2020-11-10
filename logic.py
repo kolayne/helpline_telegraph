0278cfc80984adf108eaf4a68d62bb6e465a0cf3
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2020-11-10 11:31:10 +0300

    Added ability for operators to cry (become clients)
    Also fixed docs indentation and added a warning to the docs of `in_conversation_as`

--- logic.py
+++ logic.py
@@ -6,21 +6,36 @@ from db_connector import PrettyCursor
 
 def add_user(tg_id: int) -> None:
     with PrettyCursor() as cursor:
-        cursor.execute("INSERT INTO users(tg_id, type) VALUES (%s , 'client') ON CONFLICT DO NOTHING", (tg_id,))
+        cursor.execute("INSERT INTO users(tg_id) VALUES (%s) ON CONFLICT DO NOTHING", (tg_id,))
 
 
+"""  # Not removing this part yet, because it might be useful later
 def is_operator(tg_id: int) -> bool:
     with PrettyCursor() as cursor:
-        cursor.execute("SELECT type='operator' FROM users WHERE tg_id=%s", (tg_id,))
+        cursor.execute("SELECT is_operator FROM users WHERE tg_id=%s", (tg_id,))
+        return cursor.fetchone()[0]
+"""
+
+def is_operator_and_is_not_crying(tg_id: int) -> bool:
+    with PrettyCursor() as cursor:
+        cursor.execute("SELECT user_is_operator(%s) AND NOT operator_is_crying(%s)", (tg_id, tg_id))
         return cursor.fetchone()[0]
 
 def in_conversation_as(tg_client_id: int) -> Union[str, None]:
     """
     Check if the user is in a conversation as a client, as an operator, or not in a conversation
 
+    Note that this function is not thread-safe. It's possible that the user has been in a conversation as a client and
+    becomes then an operator, it's possible, that the function will return `None`. However, such situation is impossible
+    due to logical invariants of users existence (it is only possible that a user first leaves a conversation and then
+    joins a new onr with a new role, but if both happens very fast, it's ok if we are told that the user is not in a
+    conversation, because we would get the same result from a perfect thread-safe atomic function if we have called it
+    at the moment when the user has left one conversation, but hasn't yet joined another one, so I don't think this is
+    an issue
+
     :param tg_client_id: Telegram id of the user to search for
     :return: `'operator'` if chatting as an operator, `'client'` if chatting as a client, `None` if not in a
-            conversation
+        conversation
     """
     with PrettyCursor() as cursor:
         cursor.execute("SELECT EXISTS(SELECT 1 FROM conversations WHERE operator_id=%s)", (tg_client_id,))
@@ -36,14 +51,17 @@ def start_conversation(tg_client_id: int) -> Tuple[int, int]:
     Start conversation with an operator
 
     :param tg_client_id: Telegram id of the user starting a conversation
-    :return: If the given user is in a conversation already, `(-1, -1)` is returned. If the given user is not a client,
-            `(-2, -2)` is returned. Otherwise a tuple where first element is the <b>telegram</b> operator id and the
-            second is the <b>local</b> client id
+    :return: If the given user is in a conversation already, `(-1, -1)` is returned. If the given user is in
+        conversation(s) as an operator (an operator who is currently a client), `(-2, -2)` is returned. Otherwise a
+        tuple where first element is the <b>telegram</b> operator id and the second is the <b>local</b> client id
     """
     with PrettyCursor() as cursor:
         try:
             cursor.execute("INSERT INTO conversations(client_id, operator_id) SELECT %s, tg_id FROM users WHERE "
-                           "type='operator' ORDER BY random() LIMIT 1", (tg_client_id,))
+                           "user_is_operator(tg_id) AND NOT operator_is_crying(tg_id) AND %s != tg_id ORDER BY "
+                           "random() LIMIT 1",
+                           (tg_client_id, tg_client_id))
+            # TODO: handle case when no operators were selected for the conversation
         except psycopg2.errors.UniqueViolation:
             return -1, -1
         except psycopg2.errors.CheckViolation:
@@ -64,8 +82,8 @@ def end_conversation(tg_client_id: int) -> Union[Tuple[int, int], None]:
 
     :param tg_client_id: Telegram id of the client ending the conversation
     :return: If there is no conversation with the given user as a client, `None` is returned. Otherwise a tuple of two
-            elements is returned, where the first element is the <b>telegram</b> id of the operator of the conversation,
-            the second is the <b>local</b> client id
+        elements is returned, where the first element is the <b>telegram</b> id of the operator of the conversation,
+        the second is the <b>local</b> client id
     """
     with PrettyCursor() as cursor:
         cursor.execute("SELECT operator_id, (SELECT local_id FROM users WHERE tg_id=%s) FROM conversations WHERE "
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import telebot
 from typing import Callable
 
 from db_connector import PrettyCursor
-from logic import add_user, start_conversation, end_conversation, is_operator, in_conversation_as, get_operator_id
+from logic import add_user, start_conversation, end_conversation, is_operator_and_is_not_crying, in_conversation_as,\
+    get_operator_id
 from config import bot_token
 
 
@@ -57,8 +58,8 @@ def start_conversation_handler(message: telebot.types.Message):
     if operator_id == -1:
         bot.reply_to(message, "Вы уже в беседе с оператором. Используйте /end_conversation чтобы прекратить")
     elif operator_id == -2:
-        bot.reply_to(message, "Операторы не могут запрашивать помощь :(\nОбратитесь к @kolayne для реализации такой "
-                              "возможности")
+        bot.reply_to(message, "Операторы не могут запрашивать помощь, пока помогают кому-то\nОбратитесь к @kolayne для "
+                              "реализации такой возможности")
     else:
         bot.reply_to(message, "Началась беседа с оператором. Отправьте сообщение, и оператор его увидит. "
                               "Используйте /end_conversation чтобы прекратить")
@@ -67,7 +68,7 @@ def start_conversation_handler(message: telebot.types.Message):
 @bot.message_handler(commands=['end_conversation'])
 @nonfalling_handler
 def end_conversation_handler(message: telebot.types.Message):
-    if is_operator(message.chat.id):
+    if is_operator_and_is_not_crying(message.chat.id):
         bot.reply_to(message, "Оператор не может прекратить беседу. Обратитесь к @kolayne для реализации такой "
                               "возможности")
         return
@@ -80,16 +81,16 @@ def end_conversation_handler(message: telebot.types.Message):
     else:
         bot.reply_to(message, "В данный момент вы ни с кем не беседуете. Используйте /start_conversation чтобы начать")
 
-@bot.message_handler(func=lambda message: not is_operator(message.chat.id) and
-                                          in_conversation_as(message.chat.id) is None)
-@nonfalling_handler
-def conversation_not_started(message: telebot.types.Message):
-    bot.reply_to(message, "Чтобы начать общаться с оператором, нужно написать /start_conversation")
-
 @bot.message_handler(content_types=['text'])
 @nonfalling_handler
 def text_message_handler(message: telebot.types.Message):
-    if is_operator(message.chat.id):
+    user_in_conversation_type = in_conversation_as(message.chat.id)
+
+    if user_in_conversation_type is None:
+        bot.reply_to(message, "Чтобы начать общаться с оператором, нужно написать /start_conversation")
+        return
+
+    if user_in_conversation_type == 'operator':
         if message.reply_to_message is None:
             bot.reply_to(message, "Операторы должен отвечать на сообщения. Нельзя написать сообщение просто так")
             return
@@ -105,11 +106,13 @@ def text_message_handler(message: telebot.types.Message):
                 return
 
         sent = bot.send_message(chat_id, message.text, reply_to_message_id=message_id)
-    else:
+    elif user_in_conversation_type == 'client':
         if message.reply_to_message is not None:
             pass
 
         sent = bot.send_message(get_operator_id(message.chat.id), message.text)
+    else:
+        raise NotImplementedError("Unknown `user_in_conversation_type`")
 
     with PrettyCursor() as cursor:
         cursor.execute("INSERT INTO reflected_messages(sender_chat_id, sender_message_id, receiver_chat_id, "
