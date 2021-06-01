8edd1c06fa3513cf6d6f028d84eeae31d0e77312
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2021-06-01 17:27:37 +0300

    Code restructurings post- step 3/5: redesigned `ChatBotCore`
    Made all the controllers' methods accessible directly from a `ChatBotCore` object (pseudo-inheritance, as I call it). This is done in order to let `ChatBotCore` "override" some methods of controllers to provide additional features (see the comments)

--- core/chat_bot_core.py
+++ core/chat_bot_core.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from .db_connector import DatabaseConnectionPool
 from .users import UsersController
 from .conversations import ConversationsController
@@ -5,8 +7,32 @@ from .invitations import InvitationsController
 
 
 class ChatBotCore:
-    def __init__(self, db_host, db_name, db_username, db_password):
-        self._conn_pool = DatabaseConnectionPool(db_host, db_name, db_username, db_password)
-        self.users_controller = UsersController(self._conn_pool)
-        self.conversations_controller = ConversationsController(self._conn_pool)
-        self.invitations_controller = InvitationsController(self.users_controller, self.conversations_controller)
+    # When working on #37, (among other things?) I'm going to "override" `begin_conversation`/`end_conversation`
+    # methods, so that `ChatBotCore` will interact with invitations controller too, when changing some conversation's
+    # state. So, when working on #33, TODO: in the class's docs mention, why these overridden function exist, and where
+    #                                  the border between `ChatBotCore`'s and controllers' responsibilities is.
+
+    def __init__(self, db_host: str, db_name: str, db_username: str, db_password: str):
+        conn_pool = DatabaseConnectionPool(db_host, db_name, db_username, db_password)
+        self._users_controller = UsersController(conn_pool)
+        self._conversations_controller = ConversationsController(conn_pool)
+        self._invitations_controller = InvitationsController(self._users_controller, self._conversations_controller)
+
+    def __dir__(self) -> Set[str]:
+        # Pretend that besides the attributes the object really has and the overridden methods, it also has the methods
+        # defined in the controllers
+        return set().union(
+            super().__dir__(),
+            dir(self._users_controller),
+            dir(self._conversations_controller),
+            dir(self._invitations_controller)
+        )
+
+    def __getattr__(self, item):
+        for controller in (self._users_controller, self._conversations_controller, self._invitations_controller):
+            if hasattr(controller, item):
+                return controller.__getattribute__(item)
+
+        # If reached this point, then `item` is not defined in any of the controllers. Produce `AttributeError`
+        # (by calling `object.__getattr__`):
+        return super().__getattr__(item)
